8eb63f29781745e873d331268afb47882481d590
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-02 18:29:40 -0300

    test(signupcontroller): ensure returns 400 and error if no email is provided

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -3,17 +3,19 @@ import { SignUpController } from './SignUpController'
 describe('SignUp Controller', () => {
   describe('when create user', () => {
     const sut = new SignUpController()
-    const httpRequestDefault = {
-      body: {
-        email: 'any_email',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
-    }
+    let httpRequest
+    let httpResponse
+
     describe('and no name is provided', () => {
-      let httpResponse
       beforeAll(() => {
-        httpResponse = sut.handle(httpRequestDefault)
+        httpRequest = {
+          body: {
+            email: 'any_email',
+            password: 'any_password',
+            passwordConfirmation: 'any_password'
+          }
+        }
+        httpResponse = sut.handle(httpRequest)
       })
       it('should status code return 400', () => {
         expect(httpResponse.statusCode).toBe(400)
@@ -21,6 +23,26 @@ describe('SignUp Controller', () => {
 
       it('should body return error', () => {
         expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+      })
+    })
+
+    describe('and no email is provided', () => {
+      beforeAll(() => {
+        httpRequest = {
+          body: {
+            name: 'any_name',
+            password: 'any_password',
+            passwordConfirmation: 'any_password'
+          }
+        }
+        httpResponse = sut.handle(httpRequest)
+      })
+      it('should status code return 400', () => {
+        expect(httpResponse.statusCode).toBe(400)
+      })
+
+      it('should body return error', () => {
+        expect(httpResponse.body).toEqual(new Error('Missing param: email'))
       })
     })
   })
