5ff6152db16c216f3ef88c9de242c27b0233f825
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-03 10:49:08 -0300

    feat(signupcontroller): ensure returns 400 if no password confirmation is provided

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -5,7 +5,7 @@ import { HttpRequest, HttpResponse } from '../protocols/Http'
 
 export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ['name', 'email', 'password']
+    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
     for (const field of requiredFields) {
       if (!Object.keys(httpRequest.body).includes(field)) {
