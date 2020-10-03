597f29c2df8a5e512a4c4d7c5b5400ef9d9faab5
Ygor Galeno <ygor.galeno@klickpages.com.br>
2020-10-03 10:37:54 -0300

    refactor: simplify code

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -4,16 +4,14 @@ import { HttpRequest, HttpResponse } from '../protocols/Http'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    let error: Error = new Error()
+    const requiredFields = ['name', 'email']
 
-    if (!httpRequest.body.name) {
-      error = new MissingParamError('name')
+    for (const field of requiredFields) {
+      if (!Object.keys(httpRequest.body).includes(field)) {
+        return badRequest(new MissingParamError(field))
+      }
     }
 
-    if (!httpRequest.body.email) {
-      error = new MissingParamError('email')
-    }
-
-    return badRequest(error)
+    return { statusCode: 200, body: 'ok' }
   }
 }
